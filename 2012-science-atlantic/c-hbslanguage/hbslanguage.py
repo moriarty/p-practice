ee91b27d8b729a5b7d6f3204b29d72bc2fa207f0
moriarty <alexander@dal.ca>
2012-10-24 16:26:42 -0300

    start problem c, in python

--- 2012-science-atlantic/c-hbslanguage/hbslanguage.py
+++ 2012-science-atlantic/c-hbslanguage/hbslanguage.py
@@ -3,6 +3,21 @@ Problem C: HBS Language
 '''
 
 def main():
+    try: 
+        # Get the number of sample strings
+        n = int(input())
+    except NameError:
+        print("Not an integer!")
+        return -1
+    ostr = ''
+    for i in range(n):
+        # For each string. (Maybe add try except later)
+        istr = input()
+        ostr += str(istr) #hbs_valid(s)
+        if i < n-1:
+            ostr += '\n'
+    
+    return ostr
 
 if __name__ == '__main__':
-    main()
+    print main()
